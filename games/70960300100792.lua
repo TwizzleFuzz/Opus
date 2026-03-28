b4edbec058d78e298bdb030c45854b8cb383effb
ovsepantamerlan36 <ovsepantamerlan36@gmail.com>
2026-03-28 22:32:13 +0300

    Update 70960300100792.lua

--- games/70960300100792.lua
+++ games/70960300100792.lua
@@ -5,8 +5,37 @@ local Yellow = Color3.fromHex("#ECA201")
 local Green = Color3.fromHex("#10C550")
 local Blue = Color3.fromHex("#257AF7")
 
+local autoClickDropper = false
+local autoCollectCoins = false
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Remotes = ReplicatedStorage:WaitForChild("Remotes")
+local DropperClickRemote = Remotes:WaitForChild("DropperClick")
+local CollectPartRemote = Remotes:WaitForChild("CollectPart")
+
+task.spawn(function()
+	while true do
+		if autoClickDropper then
+			pcall(function()
+				DropperClickRemote:FireServer()
+			end)
+		end
+		task.wait()
+	end
+end)
+
+local function tryCollect(obj)
+	if autoCollectCoins and obj:IsA("BasePart") and string.match(obj.Name, "^%d+$") then
+		pcall(function()
+			CollectPartRemote:FireServer(obj.Name)
+		end)
+	end
+end
+
+workspace.DescendantAdded:Connect(tryCollect)
+
 local Window = WindUI:CreateWindow({
-	Title = "Roblox Script",
+	Title = "Opus | Dropper Incremental",
 	Icon = "solar:gamepad-bold",
 	NewElements = true,
 	HideSearchBar = false,
@@ -21,6 +50,12 @@ local Window = WindUI:CreateWindow({
 		Enabled = true, 
 		Draggable = true,
 	},
+})
+
+Window:Tag({
+	Title = "Beta",
+	Color = Color3.fromHex("#1c1c1c"),
+	Border = true,
 })
 
 local MainSection = Window:Section({
@@ -41,7 +76,7 @@ local DropperGroup = MainTab:Group()
 DropperGroup:Toggle({
 	Title = "Auto Click dropper",
 	Callback = function(state)
-		print("Auto Click dropper state:", state)
+		autoClickDropper = state
 	end,
 })
 
@@ -50,7 +85,6 @@ local TreeGroup = MainTab:Group()
 TreeGroup:Toggle({
 	Title = "Auto Chop tree",
 	Callback = function(state)
-		print("Auto Chop tree state:", state)
 	end,
 })
 
@@ -59,16 +93,15 @@ local CoinsGroup = MainTab:Group()
 CoinsGroup:Toggle({
 	Title = "Auto Collect coins",
 	Callback = function(state)
-		print("Auto Collect coins state:", state)
+		autoCollectCoins = state
+		if state then
+			for _, obj in pairs(workspace:GetDescendants()) do
+				tryCollect(obj)
+			end
+		end
 	end,
 })
 
 pcall(function()
 	MainTab:Select()
 end)
-
-WindUI:Notify({
-	Title = "iOS UI Loaded",
-	Content = "Main UI in iOS style has been loaded!",
-	Duration = 5,
-})
